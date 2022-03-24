4d10697a4c374a5a278b2cef4d3d60ddc14f3144
21BCA01 <96808007+21BCA01@users.noreply.github.com>
2022-03-24 11:21:47 +0530

    this program has error

---
+++ solution 18.py
@@ -0,0 +1,44 @@
+'''WAS to enter 5 string in a list and check and print
+string whose length has even number of character'''
+count=0
+l=[]
+def createlist():
+    for i in range(1,6):
+        s=input("enter any string:")
+        l.append(s)
+
+def even(l):
+    count=0
+    for i in l:
+        for j in i:
+            count+=1
+            if len(l)%2==0:
+                print(l)
+    return(count)
+createlist()
+ans=count(even(l))
+print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+        
+
+    
+    
+
+    
